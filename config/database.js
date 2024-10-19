3bef8afe11a0813c1acbb9b08d9450386b9b7b05
Chris Nelson <chrisnelson7791@gmail.com>
2024-10-19 12:15:28 -0700

    remove useUnifiedTopology because its also deprecated

--- config/database.js
+++ config/database.js
@@ -4,7 +4,6 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
     })
 
     console.log(`MongoDB Connected: ${conn.connection.host}`)
